2346d64f4e68660821bbd54f22dae57cf6e6a284
Vinicius-D-Lella <viniciusdilella46@gmail.com>
2025-07-11 09:25:15 -0300

    html do /mbl

--- src/app/mbl/page.tsx
+++ src/app/mbl/page.tsx
@@ -1,10 +1,78 @@
 export default function mbl() {
     return (
-                <p>O Movimento Brasil Livre rapidamente se tornou um dos grupos de militância política mais relevantes do país e, em nossa trajetória de mais de uma década, expandimos nossa missão geracional para um partido político, escolas de formação intelectual, uma revista física e um aplicativo que reúne a elite do pensamento brasileiro, criando nossa própria tecnologia para promover conexões e transformações dentro da nossa comunidade.
-Enquanto movimento, somos uma organização independente que reúne milhares de membros em todos os estados da federação, com o propósito de tirar o país do atoleiro, formando, forjando e renovando os quadros que devem assumir posições de liderança na política local e nacional, deixando um legado extraordinário para a história do Brasil.
-Atualmente, contamos com mais de dez parlamentares que exercem seus mandatos com excelência e impactam efetivamente a vida das pessoas em suas cidades, estados e em todo o país. Em breve, eles serão os rostos orgulhosos do partido Missão. 
-No campo cultural, produzimos programas diários que dialogam ao vivo com dezenas de milhares de pessoas e alcançam mais de dois milhões de espectadores todos os meses.
-Nossa revista impressa já soma dezenas de milhares de exemplares, sendo folheada mensalmente pela nova elite intelectual que estamos formando.
-Com uma visão ousada e compromisso inabalável, seguimos transformando a política e a cultura do Brasil. O futuro já começou, e você pode fazer parte dessa mudança.</p>
-    );
+    <div className="mbl-page">
+
+        <div className="mbl-header">
+            <div className="mbl-header-image">
+                <h1>MBL</h1>
+            </div>
+            <div className="mbl-header-image">
+                <img src="https://framerusercontent.com/images/nvW0f3GvV8hPHn7bHKo8EpAXA.webp?scale-down-to=1024" alt="Imagem do Movimento Brasil Livre" />
+            </div>
+        </div>
+        <div className="mbl-content">
+            <p>
+                O Movimento Brasil Livre rapidamente se tornou um dos grupos de militância política mais relevantes do país e, em nossa trajetória de mais de uma década, expandimos nossa missão geracional para um partido político, escolas de formação intelectual, uma revista física e um aplicativo que reúne a elite do pensamento brasileiro, criando nossa própria tecnologia para promover conexões e transformações dentro da nossa comunidade.
+            </p>
+            <p>
+                Enquanto movimento, somos uma organização independente que reúne milhares de membros em todos os estados da federação, com o propósito de tirar o país do atoleiro, formando, forjando e renovando os quadros que devem assumir posições de liderança na política local e nacional, deixando um legado extraordinário para a história do Brasil.
+            </p>
+            <p>
+              Atualmente, contamos com mais de dez parlamentares que exercem seus mandatos com excelência e impactam efetivamente a vida das pessoas em suas cidades, estados e em todo o país. Em breve, eles serão os rostos orgulhosos do partido Missão.   
+            </p>   
+            <p>
+               No campo cultural, produzimos programas diários que dialogam ao vivo com dezenas de milhares de pessoas e alcançam mais de dois milhões de espectadores todos os meses. 
+            </p>   
+            <p>
+                Nossa revista impressa já soma dezenas de milhares de exemplares, sendo folheada mensalmente pela nova elite intelectual que estamos formando.
+            </p>   
+            <p>
+               Com uma visão ousada e compromisso inabalável, seguimos transformando a política e a cultura do Brasil. O futuro já começou, e você pode fazer parte dessa mudança. 
+            </p>   
+        </div>
+
+        <div className="mbl-bar">
+            <div className="mbl-bar-image">
+                <img src="https://framerusercontent.com/images/tzpF0RVLIyYeN8sUQkAd6uR2lQ.png" alt="dedo apontado pra você" />
+            </div>
+            <div className="mbl-bar-blank">
+            </div>
+            <div className="mbl-bar-cyan">
+                <div className="mbl-bar-cyan-content">
+                    <div className="mbl-bar-cyan-left">
+                    </div>
+                    <div className="mbl-bar-cyan-right">
+                    </div>
+                </div>
+            </div>    
+        </div>
+
+        <div className="mbl-timeline-card">
+            <h2 className="mbl-title">Timeline do Movimento Brasil Livre</h2>
+            <div className="mbl-timeline">
+                <div className="mbl-timeline-2014">
+                </div>
+                <div className="mbl-timeline-2015">
+                </div>
+                <div className="mbl-timeline-2016">
+                </div>
+                <div className="mbl-timeline-2017">
+                </div>
+                <div className="mbl-timeline-2018">
+                </div>
+                <div className="mbl-timeline-2019">
+                </div>
+                <div className="mbl-timeline-2020">
+                </div>
+                <div className="mbl-timeline-2021">
+                </div>
+                <div className="mbl-timeline-2022">
+                </div>
+                <div className="mbl-timeline-2023">
+                </div>
+                <div className="mbl-timeline-2024">
+                </div>
+            </div>
+        </div>
+    </div>);
 }
