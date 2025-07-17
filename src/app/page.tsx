61b76c9f6d49506365ccbb46bde02035cb6029af
Vinicius-D-Lella <viniciusdilella46@gmail.com>
2025-07-17 18:52:05 -0300

    atualização home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,51 @@
 export default function Home() {
-  return (
-        <section id="porta-vozes">
-            <div className="porta-vozes-left">
-                <h1>Porta Vozes</h1>
-                <p>O Porta Vozes é um projeto do MBL que visa dar voz a pessoas comuns, 
-                   permitindo que compartilhem suas histórias e experiências. Acreditamos 
-                   que cada voz tem o poder de inspirar e transformar.</p>
+    
+  return (<div>
+
+    <div className="header">
+        <div className="header-rectangle">
+            <div className="header-rectangle-container">
+                <img className="rectangle" src="https://framerusercontent.com/images/z3qpZ2QmgdGNizf3SkQhsOJyc8.png?scale-down-to=2048"></img>
             </div>
-            <div className="porta-vozes-right">
-                <button type="button">Saiba mais</button>
+        </div>
+        <div className="header-texts">
+            <div className="header-title">
+                <h1>Juntos pelo Brasil que Sonhamos!</h1>
             </div>
-        </section>
-  );
+            <div className="header-description">
+                <p>Unimos pessoas que desejam marcar sua geração e entregamos a elas oportunidades reais de agir hoje, para começarem a construir um legado único para o seu país, em cada uma das nossas iniciativas.</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                                <img src="https://framerusercontent.com/images/0uU0LqPCV0h7qFEFjBRg4QbHT8.png?scale-down-to=1024"></img>
+            </div>
+        </div>
+        <div className="header-video">
+            
+        </div>
+    </div>
+    <div className="Festival"></div>
+    <div className="Iniciativa1"></div>
+    <div className="Iniciativa2"></div>
+    <div className="Porta-vozes"></div>
+    <div className="Noticias"></div>
+    <div className="Timeline"></div>
+    <div className="Loja-mbl"></div>
+    
+  </div>);
 }
+
+//        HEADER COM VIDEO 
+
+//        CARD FESTIVAL 
+
+//        INICIATIVAS 
+
+//       PORTA VOZES 
+
+//        NOTICIAS 
+
+//        TIMELINE 
+
+//        LOJA MBL 
